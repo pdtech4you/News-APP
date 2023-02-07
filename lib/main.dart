13a6fcd6b6d0ff3d4f9c016146258aa0e9e353f7
pawan <pdtech4you@gmail.com>
2023-02-07 11:34:41 +0530

    First Screen Ui fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/news_page.dart';
-
+import 'package:sizer/sizer.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,13 +11,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
       title: 'Pawan News App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NewsPage(),
+      home:const NewsPage(),
     );
+    },);
+    
   }
 }
--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../bloc/news_bloc.dart';
 import '../models/news_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
  
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -23,10 +25,12 @@ super.initState();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+      backgroundColor: Color(0Xff464646),
+      appBar: AppBar(backgroundColor:Color(0xFF000000) ,
         title: Center(
-          child: const Text('HEADLINES')
+          child: Text('HEADLINES',
+           style: GoogleFonts.robotoSlab(color: Color(0xFFffffff),
+            fontSize: 29.sp, fontWeight: FontWeight.bold,)  ,)
         )
         ),
       body: _buildListNews(),
@@ -67,7 +71,6 @@ if (state is NewsInitial){
   return Container();
 }
 
-
     } 
    ),
       ),
@@ -80,34 +83,42 @@ Widget _buildCard(BuildContext context , NewsModel model){
     itemCount: model.articles!.length,
     itemBuilder: (context, index) {
       return Container(
-        margin: const EdgeInsets.all(8.0),
-        padding: const EdgeInsets.all(15.0),
+        margin: const EdgeInsets.only(top: 16,left: 16,right: 16,bottom: 24),
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: const Color.fromARGB(255, 196, 193, 193),width: 0.5)
+          //color: Colors.white,
+          image: DecorationImage(image:NetworkImage(model.articles![index].urlToImage.toString()),
+          fit: BoxFit.fill,
+            ),
+          borderRadius: BorderRadius.circular(5),),
+        child: Padding(
+          padding:  EdgeInsets.only(top: 50.sp, bottom: 12 , left: 5),
+          child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Text("Articles: ${model.articles![index].title}".toUpperCase(),  
+              style: GoogleFonts.robotoSlab(color: Color(0xFFf2f2f2),
+                       fontSize: 20.sp, fontWeight: FontWeight.w400,) ),
+                       
+             SizedBox(height: 24,),        
+
+             
+               Text("Articles: ${model.articles![index].author}".toUpperCase(),
+               style: GoogleFonts.robotoSlab(color: Color(0xFFbababa),
+                   fontSize: 12.sp, fontWeight: FontWeight.w700,)),
+         
+            
+            ],
+
+          ),
+         
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          
-          children: <Widget> [
-   Text("Articles: ${model.articles![index].title}".toUpperCase(),),
-   SizedBox(height: 10,),
-Image.network(model.articles![index].urlToImage.toString()),
-  Text("Articles: ${model.articles![index].description}".toUpperCase(),),
-   SizedBox(height: 10,),
-
-     Text("Articles: ${model.articles![index].author}".toUpperCase(),),
-   SizedBox(height: 10,),
-
-        ]),
       );
     },
     
     );
 }
-
 
 Widget _buildLoading() => const Center (child: CircularProgressIndicator());
 
